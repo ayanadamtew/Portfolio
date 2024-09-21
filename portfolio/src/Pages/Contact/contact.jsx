8818de58c3369b3c59f04229195a9809f30038c5
ayanadamtew <139906412+ayanadamtew@users.noreply.github.com>
2024-09-21 21:11:24 +0300

    Updated portfolio

--- portfolio/src/Pages/Contact/contact.jsx
+++ portfolio/src/Pages/Contact/contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BsInfoCircleFill } from 'react-icons/bs';
 import PageHeaderContent from '../../Component/pageHeaderContent';
+import { Animate } from 'react-simple-animate';
+import './contact.css';
 
 const Contact = () => {
   return (
@@ -9,6 +11,66 @@ const Contact = () => {
         headerText="Contact"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="contact_content">
+        <Animate
+          play
+          duration={1}
+          delay={0}
+          start={{ transform: 'translateX(-200px)' }}
+          end={{ transform: 'translateX(0px)' }}
+          v
+        >
+          <h3 className="text">Let's Talk</h3>
+        </Animate>
+        <Animate
+          play
+          duration={1}
+          delay={0}
+          start={{ transform: 'translateX(200px)' }}
+          end={{ transform: 'translateX(0px)' }}
+          v
+        >
+          <div className="form">
+            <div className="controlsWrapper">
+              <div>
+                <input
+                  required
+                  name="name"
+                  className="inputName"
+                  type="text"
+                />
+                <label htmlFor="name" className="nameLabel">
+                  Name:
+                </label>
+              </div>
+              <div>
+                <input
+                  required
+                  name="email"
+                  className="inputEmail"
+                  type="text"
+                />
+                <label htmlFor="name" className="emailLabel">
+                  Email:
+                </label>
+              </div>
+              <div>
+                <textarea
+                  required
+                  name="decription"
+                  className="inputDescription"
+                  type="text"
+                  rows={5}
+                />
+                <label htmlFor="name" className="descriptionLabel">
+                  Description:
+                </label>
+              </div>
+            </div>
+            <button> Submit</button>
+          </div>
+        </Animate>
+      </div>
     </section>
   );
 };
